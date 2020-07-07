43cd1792e231f28a63155f85f97a641e11499e55
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-07 07:15:05 +0300

    feat: add logic to transform props.ingredients to array of BurgerIngredient components

--- src/containers/BurgerBuilder/BulgerBuilder.js
+++ src/containers/BurgerBuilder/BulgerBuilder.js
@@ -4,14 +4,19 @@ import Auxilliary from '../../hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props)
-    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
 
     render() {
         return (
             <Auxilliary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Buld Controls</div>
             </Auxilliary>
         );
