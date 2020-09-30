265ec3b00a7dba92e401a99c997f2056f7bb6e95
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-09-30 22:55:26 +0300

    style: formatting, added missing semi colons

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.css'
 
 class BurgerIngredient extends Component {
-
     render() {
         let ingredient = null;
         switch (this.props.type) {
@@ -20,16 +19,16 @@ class BurgerIngredient extends Component {
                 )
                 break;
             case ('meat'):
-                ingredient = <div className={classes.Meat}>  </div>
+                ingredient = <div className={classes.Meat}></div>
                 break;
             case ('cheese'):
-                ingredient = <div className={classes.Cheese}>  </div>
+                ingredient = <div className={classes.Cheese}></div>
                 break;
             case ('bacon'):
-                ingredient = <div className={classes.Bacon}>  </div>
+                ingredient = <div className={classes.Bacon}></div>
                 break;
             case ('salad'):
-                ingredient = <div className={classes.Salad}>  </div>
+                ingredient = <div className={classes.Salad}></div>
                 break;
             default:
                 ingredient = null;
