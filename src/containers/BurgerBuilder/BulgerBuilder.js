e39e73ccaeb136463d1e4f8cd33368c15a28418e
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-10-04 19:25:47 +0300

    feat: added firebase to the app and save orders in firebase

--- src/containers/BurgerBuilder/BulgerBuilder.js
+++ src/containers/BurgerBuilder/BulgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -74,7 +75,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!')
+        // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            custommer: {
+                name: 'Stanimir',
+                address: {
+                    street: 'novaVarna',
+                    zipCode: '9010',
+                    country: 'Bulgaria'
+                },
+                email: 'test@test.bg'
+            },
+            deliveryMethod: 'takeaway'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
