c54cb8974c61c9b509b1a00d2e2615ee0a29c32b
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-10-07 08:49:09 +0300

    feat: fetching orders from database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,17 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import ErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-
+  state = {
+    orders: [],
+    loading: true
+  }
+  componentDidMount() {
+    axios.get('orders.json')
+      .then(response => {
+        const fetchedOrders = Object.keys(response.data)
+          .map(key => { return { ...response.data[key], id: key } });
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      })
+  }
   render() {
+    const orders = this.state.loading
+      ? <Spinner />
+      : this.state.orders.map(order => <Order order={order} />)
     return (
       <div>
-        <Order/>
-        <Order/>
+        {orders}
       </div>
     )
   };
 };
 
-export default Orders;
+export default ErrorHandler(Orders, axios);
