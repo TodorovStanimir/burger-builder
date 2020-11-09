b5cf5c28f9f71e7b0e57d42ec7dd431a7f40aacd
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-11-09 14:06:55 +0200

    feat: protect orders and purchase Burger with token

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -22,7 +22,7 @@ class Orders extends Component {
     //   .catch(err => {
     //     this.setState({ loading: false });
     //   })
-    this.props.onFetchOrders();
+    this.props.onFetchOrders(this.props.token);
   }
   render() {
     const orders = this.props.loading
@@ -39,13 +39,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
   return {
     orders: state.order.orders,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: () => dispatch(actions.fetchOrders())
+    onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
   };
 };
 
