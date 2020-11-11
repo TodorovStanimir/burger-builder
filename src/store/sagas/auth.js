07211fa1563d97d9b641db35ea9c292e3f2aae88
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-11-12 00:09:33 +0200

    feat: added Auth_Check_Timeout to redux-saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,6 +1,6 @@
+import { delay } from 'redux-saga/effects';
 import { put } from 'redux-saga/effects';
 
-import * as actionTypes from '../actions/actionTypes';
 import * as actions from '../actions/index';
 
 export function* logoutSaga(action) {
@@ -8,4 +8,9 @@ export function* logoutSaga(action) {
   yield localStorage.removeItem('expirationDate');
   yield localStorage.removeItem('userId');
   yield put(actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+  yield delay(action.expirationTime * 1000);
+  yield put(actions.logout());
 }
