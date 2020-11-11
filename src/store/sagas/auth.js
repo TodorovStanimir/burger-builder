84f43dc6691d470bcf7747fcc67eb9c69c0c6d8e
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-11-11 23:43:11 +0200

    feat: added redux-saga to the App and refactoring the logout

---
+++ src/store/sagas/auth.js
@@ -0,0 +1,11 @@
+import { put } from 'redux-saga/effects';
+
+import * as actionTypes from '../actions/actionTypes';
+import * as actions from '../actions/index';
+
+export function* logoutSaga(action) {
+  yield localStorage.removeItem('token');
+  yield localStorage.removeItem('expirationDate');
+  yield localStorage.removeItem('userId');
+  yield put(actions.logoutSucceed());
+}
