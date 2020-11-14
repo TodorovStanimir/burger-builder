a636a2af1966207515327f1a0126887e366be4fc
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-11-14 11:51:51 +0200

    refactor: transform Modal component to functional component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classes from './Modal.css';
 import Auxilliary from '../../../hoc/Auxilliary/Auxilliary';
 import Backdrop from '../Backdrop/Backdrop'
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props || nextProps.children !== this.props.children;
-  }
+const modal = props => {
 
-  render() {
-    return <Auxilliary>
-      <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: this.props.show ? '1' : '0'
-        }}>
-        {this.props.children}
-      </div>
-    </Auxilliary>
-  }
+  return <Auxilliary>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}>
+      {props.children}
+    </div>
+  </Auxilliary>
 }
 
-export default Modal;
+export default React.memo(modal, (prevProps, nextProps) =>
+  nextProps.show === prevProps.show &&
+  nextProps.children === prevProps.children
+);
